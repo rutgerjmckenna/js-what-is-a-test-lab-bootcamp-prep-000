8b4ce699686e6a176316e1f92de6a7043f0a2031
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-03-27 23:19:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
-var height = .toBeLessThan"74"
+var height = (toBeLessThan40)
 var message = ` is ${height} inches tall`
 
 module.exports = {name, height, message}

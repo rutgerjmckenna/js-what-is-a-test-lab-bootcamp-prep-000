58dda602e3af020fac06b1e51e11af58b9292f59
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-03-27 23:11:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // write your code below!
 var name = "Susan"
-var height = 38
+var height = "20"
 var message = ` is ${height} inches tall`
 
 module.exports = {name, height, message}
+console.log(message)
